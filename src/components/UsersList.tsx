324440dbceb845371885b2dd5dfcafef66113b22
KC Ng <nkc900201@gmail.com>
2023-07-26 23:03:44 +0900

    Remove unused  styling in Button.tsx; Add in css styling to UsersList.tsx (ul, li)

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -9,10 +9,14 @@ type UsersListProps = {
 function UsersList({ userList }: UsersListProps) {
   return (
     <Card>
-      <ul style={{ gap: '1rem' }}>
+      <ul style={{ gap: '1rem', listStyle: 'none', padding: '1rem' }}>
         {userList.map((u, i) => (
           <li
-            style={{ padding: '5px' }}
+            style={{
+              padding: '0.5rem',
+              margin: '0.5rem 0',
+              border: '1px solid #ccc',
+            }}
             key={`user_${i}`}
           >{`${u.name} (${u.age})`}</li>
         ))}
